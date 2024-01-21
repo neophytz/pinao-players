1a9572db978799fdcbacda448e89ed1787b4cd6f
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-01-21 17:12:31 +0530

    litbit changes in UI

--- src/pages/piano/Keyboard.tsx
+++ src/pages/piano/Keyboard.tsx
@@ -23,22 +23,22 @@ export const Keyboard: React.FC = () => {
 
   return (
     <React.Fragment>
-      <div className='p-10 bg-gray-800 rounded-lg text-center text-white flex items-center justify-between'>
+      <div className={`px-10 ${trackPlayed.length !== 0 ? "py-4" : "py-10"} bg-gray-800 rounded-lg text-center text-white flex items-center justify-between`}>
         <div>
           {trackPlayed.map((singleNote, index) => <span key={index}>{singleNote}</span>)}
         </div>
-        <div className="w-1/2 flex justify-end">
-          {trackPlayed.length !== 0 ? (
-            <React.Fragment>
+        {trackPlayed.length !== 0 ? (
+          <React.Fragment>
+            <div className="w-1/2 flex justify-end">
               <span onClick={() => playTrack(trackPlayed)} className='px-3 bg-gray-50 text-black rounded-md cursor-pointer hover:bg-gray-200 flex justify-center items-center'>
 
                 <svg stroke="currentColor" className='text-2xl' fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm144.1 454.9L437.7 677.8a8.02 8.02 0 0 1-12.7-6.5V353.7a8 8 0 0 1 12.7-6.5L656.1 506a7.9 7.9 0 0 1 0 12.9z"></path></svg>
 
               </span>
               <span onClick={() => setTrackPlayed([])} className='mx-2 p-3 bg-gray-50 text-black rounded-md cursor-pointer hover:bg-gray-200'>Clear</span>
-            </React.Fragment>
-          ) : ""}
-        </div>
+            </div>
+          </React.Fragment>
+        ) : ""}
       </div>
       <div className='p-2 flex justify-center align-center'>
         {
